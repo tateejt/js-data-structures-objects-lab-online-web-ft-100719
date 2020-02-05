e39678933b93a4462627e014fc9e67a32c9f6770
Emery Tate <tateejt@gmail.com>
2020-02-05 05:37:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, { [key]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, { [key]: value});
+}
